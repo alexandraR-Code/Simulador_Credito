52cc7f91f48debb51dfb0c446e31a5cf53645171
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-18 21:50:50 -0500

    feat(simulador): se implementa la función calcular y visualización del disponible mensual

--- simulador.js
+++ simulador.js
@@ -1,1 +1,13 @@
 //AQUI EL JAVASCRT PARA MANIPULAR EL HTML
+function calcular(){
+
+    // Leer ingreso y sgreso (float)
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+    //Llamar a la funcion calcularDisponible
+    let disponible = calcularDisponible(ingresos, egresos);
+
+    //Mostrar en pantalla el componente lblDisponibleValor
+    document.getElementById("spnDisponible").innerText= "USD " + disponible.toFixed(2);
+}
