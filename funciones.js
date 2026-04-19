75d0c30e5c054cea7370b32bd89aad840c8fc1cc
Alexandra Ramirez <alexa090114@gmail.com>
2026-04-18 21:05:14 -0500

    feat: agregar funcion calcularDisponible

--- funciones.js
+++ funciones.js
@@ -1,1 +1,12 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos){
+    let disponible;
+    disponible = ingresos -egresos;
+
+    if(disponible < 0){
+        return 0;
+    }
+    return disponible;
+}
+
+  
